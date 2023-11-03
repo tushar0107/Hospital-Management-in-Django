0ded1254e94ff15f0ecd010548c9a23b283522a0
tushar0107 <tushardasare1701@gmail.com>
2023-11-03 11:57:42 +0530

    ...

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#y(6ff4kz&%w8iq8nbghb3^2j%!a4dfds_9eny*^g-s=y$e=nh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
